daa47bb7b191017b79a950b79e984f239cf48a86
Joshua Cole <me@joshuacole.net>
2016-05-29 20:14:36 -0700

    Created Login

---
+++ site/accounts/jcole.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: jcole
+password: >
+  $2a$10$jYW6gvQIpYQp/UxYSPGqqeq/n0s99s/0fHWMxuQgxeaDoACQ29QM2
+email: me@joshuacole.net
+language: en
+role: admin
+history:
+  - episodes
